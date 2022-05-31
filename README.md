75bc948df83f4076e93a7bdf6becec20b84e0eaf
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-05-31 02:36:59 -0400

    Modernize and simplify code

--- README.md
+++ README.md
@@ -8,16 +8,9 @@ Read through these two resources before posting issues to GitHub or the forums.
 * [troubleshooting guide](https://github.com/custom-cards/upcoming-media-card/blob/master/troubleshooting.md)
 * [@thomasloven's lovelace guide](https://github.com/thomasloven/hass-config/wiki/Lovelace-Plugins).
 
-
-## Supporting Development
-- :coffee:&nbsp;&nbsp;[Buy me a coffee](https://www.buymeacoffee.com/FgwNR2l)
-- :1st_place_medal:&nbsp;&nbsp;[Tip some Crypto](https://github.com/sponsors/maykar)
-- :heart:&nbsp;&nbsp;[Sponsor me on GitHub](https://github.com/sponsors/maykar)
-  <br><br>
-
 ## Installation:
 
-1. Install this component by copying [these files](https://github.com/custom-components/sensor.sonarr_upcoming_media/tree/master/custom_components/sonarr_upcoming_media) to `/custom_components/sonarr_upcoming_media/`.
+1. Install this component by copying [these files](https://github.com/raman325/sensor.sonarr_upcoming_media/tree/master/custom_components/sonarr_upcoming_media) to `/custom_components/sonarr_upcoming_media/`.
 2. Install the card: [Upcoming Media Card](https://github.com/custom-cards/upcoming-media-card)
 3. Add the code to your `configuration.yaml` using the config options below.
 4. Add the code for the card to your `ui-lovelace.yaml`. 
@@ -65,3 +58,7 @@ sensor:
 | line3 | $rating - $runtime | "★ 7.8 - 45 min" |
 | line4 | $number - $studio | "S06E09 - AMC"
 | icon | mdi:arrow-down-bold | https://materialdesignicons.com/icon/arrow-down-bold
+
+### Credits
+
+This integration was originally written by @maykar. Since the original repository for this integration was archived, this fork is an attempt to keep it alive.
--- custom_components/sonarr_upcoming_media/sensor.py
+++ custom_components/sonarr_upcoming_media/sensor.py
@@ -2,177 +2,147 @@
 Home Assistant component to feed the Upcoming Media Lovelace card with
 Sonarr upcoming releases.
 
-https://github.com/custom-components/sensor.radarr_upcoming_media
+https://github.com/raman325/sensor.radarr_upcoming_media
 
 https://github.com/custom-cards/upcoming-media-card
 
 """
+from datetime import date, datetime, timedelta
 import logging
-import json
-import time
-import requests
-from datetime import date, datetime
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT, CONF_SSL
-from homeassistant.helpers.entity import Entity
 
-__version__ = '0.1.10'
+from aiopyarr.exceptions import ArrException
+from aiopyarr.models.host_configuration import PyArrHostConfiguration
+from aiopyarr.sonarr_client import SonarrCalendar, SonarrClient, SonarrSeries
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT, CONF_SSL
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+
+__version__ = "0.1.10"
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_DAYS = 'days'
-CONF_URLBASE = 'urlbase'
-CONF_MAX = 'max'
+CONF_DAYS = "days"
+CONF_URLBASE = "urlbase"
+CONF_MAX = "max"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Optional(CONF_DAYS, default='7'): cv.string,
-    vol.Optional(CONF_HOST, default='localhost'): cv.string,
-    vol.Optional(CONF_PORT, default=8989): cv.port,
-    vol.Optional(CONF_SSL, default=False): cv.boolean,
-    vol.Optional(CONF_URLBASE, default=''): cv.string,
-    vol.Optional(CONF_MAX, default=5): cv.string,
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_API_KEY): cv.string,
+        vol.Optional(CONF_DAYS, default=7): vol.Coerce(int),
+        vol.Optional(CONF_HOST, default="localhost"): cv.string,
+        vol.Optional(CONF_PORT, default=8989): cv.port,
+        vol.Optional(CONF_SSL, default=False): cv.boolean,
+        vol.Optional(CONF_URLBASE): cv.string,
+        vol.Optional(CONF_MAX, default=5): vol.Coerce(int),
+    }
+)
+
+FIRST_CARD = {
+    "title_default": "$title",
+    "line1_default": "$number - $episode",
+    "line2_default": "$release",
+    "line3_default": "$rating - $runtime",
+    "line4_default": "$studio",
+    "icon": "mdi:arrow-down-bold",
+}
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     add_devices([SonarrUpcomingMediaSensor(hass, config)], True)
 
 
-class SonarrUpcomingMediaSensor(Entity):
-
+class SonarrUpcomingMediaSensor(SensorEntity):
     def __init__(self, hass, conf):
-        from pytz import timezone
-        self.host = conf.get(CONF_HOST)
-        self.port = conf.get(CONF_PORT)
-        self.urlbase = conf.get(CONF_URLBASE)
-        if self.urlbase:
-            self.urlbase = "{}/".format(self.urlbase.strip('/'))
-        self.apikey = conf.get(CONF_API_KEY)
-        self.days = int(conf.get(CONF_DAYS))
-        self.ssl = 's' if conf.get(CONF_SSL) else ''
-        self._state = None
-        self.data = []
-        self._tz = timezone(str(hass.config.time_zone))
-        self.max_items = int(conf.get(CONF_MAX))
+        url_base = conf.get(CONF_URLBASE)
+        if url_base:
+            url_base = "{}/".format(url_base.strip("/"))
+        self._host_config = PyArrHostConfiguration(
+            api_token=conf[CONF_API_KEY],
+            hostname=conf[CONF_HOST],
+            port=conf[CONF_PORT],
+            ssl=conf[CONF_SSL],
+            base_api_path=url_base,
+        )
+        self.client = SonarrClient(
+            self._host_config, session=async_get_clientsession(hass)
+        )
+        self.days = conf.get(CONF_DAYS)
+        self.max_items = conf.get(CONF_MAX)
 
-    @property
-    def name(self):
-        return 'Sonarr_Upcoming_Media'
+        self._attr_available = True
+        self._attr_name = "Sonarr Upcoming Media"
 
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def device_state_attributes(self):
-        import re
-        """Return JSON for the sensor."""
-        attributes = {}
-        default = {}
-        card_json = []
-        default['title_default'] = '$title'
-        default['line1_default'] = '$episode'
-        default['line2_default'] = '$release'
-        default['line3_default'] = '$rating - $runtime'
-        default['line4_default'] = '$number - $studio'
-        default['icon'] = 'mdi:arrow-down-bold'
-        card_json.append(default)
-        for show in self.data:
-            card_item = {}
-            if 'series' not in show:
-                continue
-            card_item['airdate'] = show['airDateUtc']
-            if days_until(show['airDateUtc'], self._tz) <= 7:
-                card_item['release'] = '$day, $time'
-            else:
-                card_item['release'] = '$day, $date $time'
-            card_item['flag'] = show.get('hasFile', False)
-            if 'title' in show['series']:
-                card_item['title'] = show['series']['title']
-            else:
-                continue
-            card_item['episode'] = show.get('title', '')
-            if 'seasonNumber' and 'episodeNumber' in show:
-                card_item['number'] = 'S{:02d}E{:02d}'.format(show['seasonNumber'],
-                                                        show['episodeNumber'])
-            else:
-                card_item['number'] = ''
-            if 'runtime' in show['series']:
-                card_item['runtime'] = show['series']['runtime']
-            else:
-                card_item['runtime'] = ''
-            if 'network' in show['series']:
-                card_item['studio'] = show['series']['network']
-            else:
-                card_item['studio'] = ''
-            if ('ratings' in show['series'] and
-               show['series']['ratings']['value'] > 0):
-                    card_item['rating'] = ('\N{BLACK STAR} ' +
-                                     str(show['series']['ratings']['value']))
-            else:
-                card_item['rating'] = ''
-            if 'genres' in show['series']:
-                card_item['genres'] = ', '.join(show['series']['genres'])
-            else:
-                card_item['genres'] = ''
-            try:
-                for img in show['series']['images']:
-                    if img['coverType'] == 'poster':
-                        card_item['poster'] = re.sub('.jpg', '_t.jpg', img['url'])
-            except:
-                continue
-            try:
-                card_item['fanart'] = ''
-                for img in show['series']['images']:
-                    if img['coverType'] == 'fanart':
-                        card_item['fanart'] = re.sub('.jpg', '_t.jpg', img['url'])
-            except:
-                pass
-            card_json.append(card_item)
-        attributes['data'] = card_json
-        return attributes
-
-    def update(self):
-        start = get_date(self._tz)
-        end = get_date(self._tz, self.days)
+    def _get_rating(self, series: SonarrSeries):
+        """Return rating."""
         try:
-            api = requests.get('http{0}://{1}:{2}/{3}api/calendar?start={4}'
-                               '&end={5}'.format(self.ssl, self.host,
-                                                 self.port, self.urlbase,
-                                                 start, end),
-                               headers={'X-Api-Key': self.apikey}, timeout=10)
-        except OSError:
-            _LOGGER.warning("Host %s is not available", self.host)
-            self._state = '%s cannot be reached' % self.host
+            return "\N{BLACK STAR} " + str(series.ratings.value)
+        except AttributeError:
+            return ""
+
+    async def async_update(self):
+        start = datetime.combine(date.today(), datetime.min.time())
+        end = start + timedelta(days=self.days)
+        try:
+            episodes = await self.client.async_get_calendar(start, end)
+        except ArrException as err:
+            if self._attr_available:
+                _LOGGER.warning(err)
+            self._attr_available = False
             return
 
-        if api.status_code == 200:
-            self._state = 'Online'
-            if self.days == 1:
-                self.data = list(filter(lambda x: x['airDate'][:-10] == str(
-                    start), api.json()))[:self.max_items]
+        if isinstance(episodes, SonarrCalendar):
+            episodes = [episodes]
+
+        episodes = [episode for episode in episodes if hasattr(episode, "series")][
+            : self.max_items
+        ]
+        self._attr_available = True
+        self._attr_native_value = len(episodes)
+        self._attr_extra_state_attributes = {"data": [FIRST_CARD]}
+
+        for episode in episodes:
+            series_list = await self.client.async_get_series(episode.seriesId)
+            if isinstance(series_list, list):
+                series = next(series for series in series_list)
             else:
-                self.data = api.json()[:self.max_items]
-        else:
-            self._state = '%s cannot be reached' % self.host
+                series = series_list
 
+            episode_data = {
+                "airdate": datetime.date(episode.airDateUtc).isoformat(),
+                "release": "$day, $date $time",
+                "flag": episode.attributes.get("hasFile", False),
+                "title": series.attributes.get("title", ""),
+                "runtime": series.attributes.get("runtime", ""),
+                "episode": episode.attributes.get("title", ""),
+                "number": "",
+                "rating": self._get_rating(series),
+                "studio": series.attributes.get("network", ""),
+                "genres": ", ".join(series.attributes.get("genres", [])),
+                "poster": "",
+                "fanart": "",
+            }
+            if hasattr(episode, "seasonNumber") and hasattr(episode, "episodeNumber"):
+                episode_data["number"] = "S{:02d}E{:02d}".format(
+                    episode.seasonNumber, episode.episodeNumber
+                )
 
-def get_date(zone, offset=0):
-    """Get date based on timezone and offset of days."""
-    return datetime.date(datetime.fromtimestamp(
-        time.time() + 86400 * offset, tz=zone))
-
-
-def days_until(date, tz):
-    from pytz import utc
-    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
-    date = str(date.replace(tzinfo=utc).astimezone(tz))[:10]
-    date = time.strptime(date, '%Y-%m-%d')
-    date = time.mktime(date)
-    now = datetime.now().strftime('%Y-%m-%d')
-    now = time.strptime(now, '%Y-%m-%d')
-    now = time.mktime(now)
-    return int((date - now) / 86400)
+            if hasattr(series, "images"):
+                episode_data["poster"] = next(
+                    (
+                        image.remoteUrl
+                        for image in series.images
+                        if image.coverType == "poster"
+                    ),
+                    "",
+                )
+                episode_data["fanart"] = next(
+                    (
+                        image.remoteUrl
+                        for image in series.images
+                        if image.coverType == "fanart"
+                    ),
+                    "",
+                )
+            self._attr_extra_state_attributes["data"].append(episode_data)
